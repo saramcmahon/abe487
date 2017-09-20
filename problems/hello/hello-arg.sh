92b6e6ba2bd193598c54091880f49b8370c20b13
Sara Marie McMahon <saramcmahon@login2.cm.cluster>
2017-09-19 23:57:18 -0700

    homework done

--- problems/hello/hello-arg.sh
+++ problems/hello/hello-arg.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $# -ne 1 ]]; then
+printf "Usage: %s NAME\n" "$(basename "$0")"
+exit 1
+fi
+
+echo "Hello, $1!"
--- problems/hello/hello-arg2.sh
+++ problems/hello/hello-arg2.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $# -ne 2 ]]; then
+printf "Usage: %s greeting name\n" "$(basename "$0")"
+exit 1
+fi
+
+echo "$1, $2!"
+
