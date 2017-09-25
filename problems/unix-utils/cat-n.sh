2d256cb4a2baa8e86533bf29ce4f57802412e509
Sara Marie McMahon <saramcmahon@login3.cm.cluster>
2017-09-25 02:14:32 -0700

    all good

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $#  -lt 1 ]]; then
+printf "Usage: cat-n.sh file" 
+exit 1
+
+fi
+
+cat -n $1
+
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $# -lt 1 ]]; then
+printf "Usage: head.sh FILE [NUM]"
+exit 1
+fi
+
+set -u
+
+FILE=$1
+NAME=${2:-3}
+
+ls | head $FILE -n$NAME
+
+
+
+
+
+
+
+
+
