f483f4b3aa357a994f0e7fdb2e5104e29943dd95
Sara Marie McMahon <saramcmahon@login2.cm.cluster>
2017-10-03 02:44:50 -0700

    adding hello-py homework

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
-"""say hello to the arguments"""
 
-print('OK')
+import sys
+import os
+
+
+def main():
+	args = sys.argv
+
+	if len(args) < 2:
+		script = os.path.basename(args[0])
+		print('Usage: {} NAME [NAME2 ...]'.format(script))
+		sys.exit(1)		
+	
+	sys.stdout.write
+	counter=0
+	counter2=0
+
+	for name in args[1:]:
+		counter += 1
+	sys.stdout.write("Hello to the {}".format(counter) + ' of you:')
+	
+	if len(args) == 3:
+		for name in args[1:]:
+			sys.stdout.write(' ' + name)
+			if counter2 == 0:
+				sys.stdout.write(' and')
+				counter2 += 1
+		sys.stdout.write('!\n')
+	if counter > 1:
+		if len(args) != 3:
+			for name in args[1:]:
+				counter2 += 1
+				if counter2 < counter:
+					sys.stdout.write(' ' + name + ',')
+			if counter2 == counter:
+				sys.stdout.write(' and ' + name + '!\n')
+
+	if counter == 1:
+		sys.stdout.write(' ' + name + '!\n')
+
+if __name__ == '__main__':
+	main()
--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-"""count the vowels in a word"""
 
-print('OK')
+import sys, string
+import sys
+import os
+	
+args = sys.argv
+
+if len(args) < 2:
+	script = os.path.basename(args[0])
+	print('Usage: vowel_counter.py STRING'.format(script))
+	sys.exit(1)
+	
+numVowels = 0
+strings = args[1:]
+
+for char in strings:
+	if char in 'aeiouAEIOU':
+		numVowels += 1
+		print('There is ' +(str(numVowels))+ 'vowels in' + "str")	
+	else:
+		numVowels += 0
+		print('There is {} '.format(numVowels) + 'vowels in' + "args[1:]")
+
