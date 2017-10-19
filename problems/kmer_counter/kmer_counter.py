badb4ed7f3a68899e43c8b3667467a532b437ff5
Sara McMahon <saramcmahon@email.arizona.edu>
2017-10-19 07:46:48 -0700

    all good

--- problems/kmer_counter/kmer_counter.py
+++ problems/kmer_counter/kmer_counter.py
@@ -1,3 +1,59 @@
 #!/usr/bin/env python3
 
-print('OK')
+from collections import Counter
+import sys, string
+import os
+import os.path
+from pathlib import Path
+
+args = sys.argv[1:]
+if len(args) < 2:
+	print("Usage: kmer_counter.py LEN STR")
+	sys.exit(1)
+
+if args[0].isdigit():
+    	kmer_size = int(args[0])
+else:
+	print('Kmer size "{}" is not a number'.format(args[0]))
+	sys.exit(1)
+
+
+if kmer_size <= 0:
+	print('Kmer size "{}" must be > 0'.format(kmer_size))
+	sys.exit(1)
+		
+input = args[1]
+nkmers = len(input) - kmer_size + 1
+
+if kmer_size > len(input):
+	print('There are no {}-mers in "{}"'.format(kmer_size, args[1]))
+	sys.exit(1)
+
+kmers = {}
+
+i = 0
+add = ""
+j = 0
+z = 0
+
+while i < nkmers:
+        add = ""
+        j = 0
+        z = i
+        while j < kmer_size:
+                add += input[z+j]
+                j += 1
+        if add in kmers:
+                kmers[add] += 1
+        else:
+                kmers[add] = 1
+        i += 1
+
+print('{}'.format(input))
+
+for x in sorted(kmers):
+        print(x, kmers[x])
+
+#else:
+#	print('input "{}" size "{}" nkmer "{}"'.format(input, kmer_size, nkmers))
+#	sys.exit()
